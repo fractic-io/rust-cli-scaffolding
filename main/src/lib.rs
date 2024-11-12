fe149590b12dffc4477ac806499358b681a4755d
Mart van Buren <mart@fractic.io>
2024-11-13 01:21:20 +0900

    [main] Make each lib_... an optional feature.

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -1,4 +1,10 @@
-pub use lib_aws::*;
 pub use lib_core::*;
+
+#[cfg(feature = "aws")]
+pub use lib_aws::*;
+
+#[cfg(feature = "flutter")]
 pub use lib_flutter::*;
+
+#[cfg(feature = "image")]
 pub use lib_image::*;
