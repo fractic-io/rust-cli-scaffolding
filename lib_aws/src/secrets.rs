8581a4393eadd72174885b0de06b7936143144eb
Mart van Buren <mart@fractic.io>
2024-11-15 08:48:16 +0900

    [aws] More AWS Secrets Manager utils.

--- lib_aws/src/secrets.rs
+++ lib_aws/src/secrets.rs
@@ -1,15 +1,36 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use aws_sdk_secretsmanager::Client;
+use aws_sdk_secretsmanager::{
+    error::SdkError, operation::describe_secret::DescribeSecretError, Client,
+};
 use lib_core::{define_cli_error, CliError};
 
 use crate::shared_config::config_from_profile;
 
 define_cli_error!(
-    AwsSecretsManagerError,
+    FailedToFetchAwsSecret,
     "Error fetching secret '{secret_id}' from AWS Secrets Manager in region '{region}': {error}.",
     { secret_id: &str, region: &str, error: &str }
 );
+
+define_cli_error!(
+    AwsSecretsManagerError,
+    "Error running AWS Secrets Manager command."
+);
+
+pub async fn secret_exists(profile: &str, region: &str, secret_id: &str) -> Result<bool, CliError> {
+    let client = Client::new(&config_from_profile(profile, region).await);
+    let response = client.describe_secret().secret_id(secret_id).send().await;
+    match response {
+        Ok(_) => Ok(true),
+        Err(SdkError::<DescribeSecretError>::ServiceError(se))
+            if se.err().is_resource_not_found_exception() =>
+        {
+            Ok(false)
+        }
+        Err(e) => Err(AwsSecretsManagerError::with_debug(&e)),
+    }
+}
 
 pub async fn get_secret(
     profile: &str,
@@ -25,23 +46,41 @@ pub async fn get_secret(
         .secret_id(secret_id)
         .send()
         .await
-        .map_err(|e| {
-            AwsSecretsManagerError::with_debug(secret_id, region, "AWS callout failed", &e)
-        })?;
+        .map_err(|e| AwsSecretsManagerError::with_debug(&e))?;
     let secrets_string = secrets_output.secret_string().ok_or_else(|| {
-        AwsSecretsManagerError::new(secret_id, region, "could not parse secret value")
+        FailedToFetchAwsSecret::new(secret_id, region, "could not parse secret value")
     })?;
     let secrets_json =
         serde_json::from_str::<HashMap<String, String>>(secrets_string).map_err(|e| {
-            AwsSecretsManagerError::with_debug(secret_id, region, "could not parse JSON", &e)
+            FailedToFetchAwsSecret::with_debug(secret_id, region, "could not parse JSON", &e)
         })?;
 
     // Fetch required keys from JSON.
     Ok(secrets_json.get(key).cloned().ok_or_else(|| {
-        AwsSecretsManagerError::new(
+        FailedToFetchAwsSecret::new(
             secret_id,
             region,
             &format!("secret did not contain key '{key}'"),
         )
     })?)
+}
+
+pub async fn secret_replication_regions(
+    profile: &str,
+    region: &str,
+    secret_id: &str,
+) -> Result<HashSet<String>, CliError> {
+    let client = Client::new(&config_from_profile(profile, region).await);
+    let response = client
+        .describe_secret()
+        .secret_id(secret_id)
+        .send()
+        .await
+        .map_err(|e| AwsSecretsManagerError::with_debug(&e))?;
+    Ok(response
+        .replication_status()
+        .into_iter()
+        .filter_map(|status| status.region())
+        .map(|region| region.to_string())
+        .collect())
 }
