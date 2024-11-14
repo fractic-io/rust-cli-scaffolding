2d48f85e540c487880cbe8e504dcfd40a3180939
Mart van Buren <mart@fractic.io>
2024-11-14 20:35:33 +0900

    [aws] Minor rename.

--- lib_aws/src/account.rs
+++ lib_aws/src/account.rs
@@ -4,7 +4,7 @@ use lib_core::{define_cli_error, CliError};
 
 define_cli_error!(AwsInvalidAccountId, "Invalid AWS account ID.");
 
-pub fn require_valid_account_id(account_id: impl Display) -> Result<u64, CliError> {
+pub fn require_aws_account_id(account_id: impl Display) -> Result<u64, CliError> {
     let account_id_str = account_id.to_string();
     if account_id_str.len() != 12 {
         return Err(AwsInvalidAccountId::with_debug(&account_id.to_string()));
