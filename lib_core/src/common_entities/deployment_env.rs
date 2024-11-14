23ad9340162e7c25ba9313a42d12a4842ce4b388
Mart van Buren <mart@fractic.io>
2024-11-14 20:09:53 +0900

    [core] common_entities: Derive EnumIter & ValueEnum.

--- lib_core/src/common_entities/deployment_env.rs
+++ lib_core/src/common_entities/deployment_env.rs
@@ -1,7 +1,10 @@
+use clap::ValueEnum;
 use serde::{Deserialize, Serialize};
+use strum_macros::EnumIter;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize, EnumIter, ValueEnum)]
 #[serde(rename_all = "snake_case")]
+#[clap(rename_all = "snake_case")]
 pub enum DeploymentEnv {
     Sandbox,
     Staging,
