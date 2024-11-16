069ddf5416cd0380abcbc685d11436d942548790
Mart van Buren <mart@fractic.io>
2024-11-16 11:21:53 +0900

    [aws] Minor improvements to Secrets Manager utils.

--- lib_aws/src/secrets.rs
+++ lib_aws/src/secrets.rs
@@ -1,9 +1,10 @@
 use std::collections::{HashMap, HashSet};
 
 use aws_sdk_secretsmanager::{
-    error::SdkError, operation::describe_secret::DescribeSecretError, Client,
+    error::SdkError, operation::describe_secret::DescribeSecretError, types::ReplicaRegionType,
+    Client,
 };
-use lib_core::{define_cli_error, CliError};
+use lib_core::{define_cli_error, CliError, Printer};
 
 use crate::shared_config::config_from_profile;
 
@@ -11,6 +12,12 @@ define_cli_error!(
     FailedToFetchAwsSecret,
     "Error fetching secret '{secret_id}' from AWS Secrets Manager in region '{region}': {error}.",
     { secret_id: &str, region: &str, error: &str }
+);
+
+define_cli_error!(
+    AwsSecretSubkeyNotFound,
+    "Secret '{secret_id}' in region '{region}' does not contain subkey {subkey}.",
+    { secret_id: &str, region: &str, subkey: &str }
 );
 
 define_cli_error!(
@@ -32,40 +39,39 @@ pub async fn secret_exists(profile: &str, region: &str, secret_id: &str) -> Resu
     }
 }
 
-pub async fn get_secret(
+pub async fn get_secret(profile: &str, region: &str, secret_id: &str) -> Result<String, CliError> {
+    let client = Client::new(&config_from_profile(profile, region).await);
+    match client.get_secret_value().secret_id(secret_id).send().await {
+        Ok(output) => Ok(output
+            .secret_string()
+            .ok_or_else(|| {
+                FailedToFetchAwsSecret::new(secret_id, region, "could not parse secret value")
+            })?
+            .to_owned()),
+        Err(e) => Err(AwsSecretsManagerError::with_debug(&e)),
+    }
+}
+
+pub async fn get_secret_subkey(
     profile: &str,
     region: &str,
     secret_id: &str,
-    key: &str,
+    subkey: &str,
 ) -> Result<String, CliError> {
-    let client = Client::new(&config_from_profile(profile, region).await);
-
     // Fetch secrets JSON.
-    let secrets_output = client
-        .get_secret_value()
-        .secret_id(secret_id)
-        .send()
-        .await
-        .map_err(|e| AwsSecretsManagerError::with_debug(&e))?;
-    let secrets_string = secrets_output.secret_string().ok_or_else(|| {
-        FailedToFetchAwsSecret::new(secret_id, region, "could not parse secret value")
+    let raw = get_secret(profile, region, secret_id).await?;
+    let json = serde_json::from_str::<HashMap<String, String>>(&raw).map_err(|e| {
+        FailedToFetchAwsSecret::with_debug(secret_id, region, "could not parse JSON", &e)
     })?;
-    let secrets_json =
-        serde_json::from_str::<HashMap<String, String>>(secrets_string).map_err(|e| {
-            FailedToFetchAwsSecret::with_debug(secret_id, region, "could not parse JSON", &e)
-        })?;
 
-    // Fetch required keys from JSON.
-    Ok(secrets_json.get(key).cloned().ok_or_else(|| {
-        FailedToFetchAwsSecret::new(
-            secret_id,
-            region,
-            &format!("secret did not contain key '{key}'"),
-        )
-    })?)
+    // Fetch required key from JSON.
+    Ok(json
+        .get(subkey)
+        .cloned()
+        .ok_or_else(|| AwsSecretSubkeyNotFound::new(secret_id, region, subkey))?)
 }
 
-pub async fn secret_replication_regions(
+pub async fn secret_replica_regions(
     profile: &str,
     region: &str,
     secret_id: &str,
@@ -83,4 +89,50 @@ pub async fn secret_replication_regions(
         .filter_map(|status| status.region())
         .map(|region| region.to_string())
         .collect())
+}
+
+/// Returns true if new secret was created.
+pub async fn update_or_create_secret(
+    pr: &Printer,
+    profile: &str,
+    region: &str,
+    secret_id: &str,
+    value: &str,
+    replica_regions: Option<&HashSet<String>>,
+) -> Result<bool, CliError> {
+    let client = Client::new(&config_from_profile(profile, region).await);
+    match secret_exists(profile, region, secret_id).await? {
+        true => {
+            pr.info(&format!("Updating secret '{secret_id}'..."));
+            client
+                .put_secret_value()
+                .secret_id(secret_id)
+                .secret_string(value)
+                .send()
+                .await
+                .map_err(|e| AwsSecretsManagerError::with_debug(&e))?;
+            pr.info("Secret updated.");
+            Ok(false)
+        }
+        false => {
+            pr.info(&format!(
+                "Creating secret '{secret_id}' with replication regions '{:?}'...",
+                replica_regions,
+            ));
+            client
+                .create_secret()
+                .name(secret_id)
+                .secret_string(value)
+                .set_add_replica_regions(replica_regions.map(|r| {
+                    r.into_iter()
+                        .map(|r| ReplicaRegionType::builder().region(r).build())
+                        .collect()
+                }))
+                .send()
+                .await
+                .map_err(|e| AwsSecretsManagerError::with_debug(&e))?;
+            pr.info("Secret created.");
+            Ok(true)
+        }
+    }
 }
