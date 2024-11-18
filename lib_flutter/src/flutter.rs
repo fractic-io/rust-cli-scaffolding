b31d04d5615ada71ee2edbb79c77f6518352f83e
Mart van Buren <mart@fractic.io>
2024-11-18 09:37:26 +0900

    [flutter] Add flutter install util.

--- lib_flutter/src/flutter.rs
+++ lib_flutter/src/flutter.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, path::Path};
 
-use lib_core::{CliError, Executor, Printer};
+use lib_core::{CliError, Executor, IOMode, Printer};
 
 pub enum BuildFor {
     Android,
@@ -38,7 +38,7 @@ pub fn run_flutter_integration_test(
         .collect();
     args.extend(setexprs.iter().map(|s| s.as_str()));
 
-    ex.execute("flutter", &args, Some(dir), lib_core::IOMode::StreamOutput)?;
+    ex.execute("flutter", &args, Some(dir), IOMode::StreamOutput)?;
 
     Ok(())
 }
@@ -46,7 +46,7 @@ pub fn run_flutter_integration_test(
 pub fn flutter_build_release(
     pr: &Printer,
     ex: &Executor,
-    app: &Path,
+    dir: &Path,
     os: BuildFor,
     flavor: Option<&str>,
 ) -> Result<(), CliError> {
@@ -54,17 +54,75 @@ pub fn flutter_build_release(
     match os {
         BuildFor::Android => {
             pr.info("Building Android app bundle...");
-            args.push("appbundle")
+            args.push("appbundle");
         }
         BuildFor::Ios => {
             pr.info("Building iOS app...");
-            args.push("ios")
+            args.push("ios");
         }
     }
     args.push("--release");
     if let Some(flavor) = flavor {
         args.extend(&["--flavor", flavor]);
     }
-    ex.execute("flutter", &args, Some(app), lib_core::IOMode::StreamOutput)?;
+    ex.execute("flutter", &args, Some(dir), IOMode::StreamOutput)?;
+    Ok(())
+}
+
+pub fn flutter_install(
+    pr: &Printer,
+    ex: &Executor,
+    dir: &Path,
+    os: BuildFor,
+    flavor: Option<&str>,
+) -> Result<(), CliError> {
+    let mut build_args = vec!["build"];
+    match os {
+        BuildFor::Android => {
+            pr.info("Building Android apk...");
+            build_args.push("apk");
+        }
+        BuildFor::Ios => {
+            pr.info("Building iOS app...");
+            build_args.push("ios");
+        }
+    }
+    build_args.push("--release");
+    if let Some(flavor) = flavor {
+        build_args.extend(&["--flavor", flavor]);
+    }
+    ex.execute(
+        "flutter",
+        &build_args,
+        Some(dir),
+        lib_core::IOMode::StreamOutput,
+    )?;
+
+    pr.info("Installing...");
+    let mut install_args = vec!["install"];
+    if let Some(flavor) = flavor {
+        install_args.extend(&["--flavor", flavor]);
+    }
+    match os {
+        BuildFor::Android => {
+            // First try directly installing with adb (to do "streamed install"
+            // if the app is already exists), but fall back to flutter install.
+            let executable_path = match flavor {
+                Some(f) => format!("build/app/outputs/flutter-apk/app-{f}-release.apk"),
+                None => "build/app/outputs/flutter-apk/app-release.apk".to_string(),
+            };
+            ex.execute(
+                "adb",
+                &["install", "-r", &executable_path],
+                Some(dir),
+                IOMode::StreamOutput,
+            )
+            .or_else(|_| ex.execute("flutter", &install_args, Some(dir), IOMode::StreamOutput))?;
+        }
+        BuildFor::Ios => {
+            ex.execute("flutter", &install_args, Some(dir), IOMode::StreamOutput)?;
+        }
+    }
+
     Ok(())
 }
