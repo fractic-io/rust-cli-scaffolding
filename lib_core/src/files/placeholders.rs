a57d2d22a6499095d45adc57e0b73211da96f326
Mart van Buren <mart@fractic.io>
2024-11-19 18:41:24 +0900

    [core] Add util to replace placeholders in String as well.

--- lib_core/src/files/placeholders.rs
+++ lib_core/src/files/placeholders.rs
@@ -2,14 +2,14 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Write};
-use std::path::{Display, PathBuf};
+use std::path::PathBuf;
 
 use crate::{define_cli_error, CliError, IOError};
 
 define_cli_error!(
     UnreplacedPlaceholdersRemain,
-    "Unexpected placeholders remain in file '{file}': {unreplaced:?}.",
-    { file: &Display<'_>, unreplaced: &Vec<String> }
+    "Unexpected placeholders remain: {unreplaced:?}.",
+    { unreplaced: &Vec<String> }
 );
 
 pub fn replace_all_placeholders_in_file(
@@ -25,13 +25,32 @@ pub fn replace_all_placeholders_in_file(
         .read_to_string(&mut file_content)
         .map_err(|e| IOError::with_debug(&e))?;
 
+    let replaced_content = replace_all_placeholders_in_string(
+        file_content,
+        placeholders,
+        error_if_unreplaced_placeholders_remain,
+    )?;
+
+    // Write the updated content back to the file.
+    let mut file = File::create(dst).map_err(|e| IOError::with_debug(&e))?;
+    file.write_all(replaced_content.as_bytes())
+        .map_err(|e| IOError::with_debug(&e))?;
+
+    Ok(())
+}
+
+pub fn replace_all_placeholders_in_string(
+    content: String,
+    placeholders: &HashMap<String, String>,
+    error_if_unreplaced_placeholders_remain: bool,
+) -> Result<String, CliError> {
     // Use a regex to find placeholders of the form {{Key}}.
     let placeholder_pattern =
         Regex::new(r"\{\{(\w+)\}\}").expect("hardcoded regex should be valid");
 
     // Replace all placeholders with their corresponding values.
     let mut unknown_keys = Vec::new();
-    let result = placeholder_pattern.replace_all(&file_content, |caps: &regex::Captures| {
+    let result = placeholder_pattern.replace_all(&content, |caps: &regex::Captures| {
         let key = &caps[1]; // The content inside {{ }}.
         if let Some(value) = placeholders.get(key) {
             value.clone()
@@ -44,16 +63,8 @@ pub fn replace_all_placeholders_in_file(
     let replaced_content = result.into_owned();
 
     if error_if_unreplaced_placeholders_remain && !unknown_keys.is_empty() {
-        return Err(UnreplacedPlaceholdersRemain::new(
-            &src.display(),
-            &unknown_keys,
-        ));
+        return Err(UnreplacedPlaceholdersRemain::new(&unknown_keys));
     }
 
-    // Write the updated content back to the file.
-    let mut file = File::create(dst).map_err(|e| IOError::with_debug(&e))?;
-    file.write_all(replaced_content.as_bytes())
-        .map_err(|e| IOError::with_debug(&e))?;
-
-    Ok(())
+    Ok(replaced_content)
 }
