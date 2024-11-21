3a498fb3fb22c876667ea60fb48375be9631cbd0
Mart van Buren <mart@fractic.io>
2024-11-21 19:16:56 +0900

    Add skeleton lib_docker.

---
+++ lib_docker/src/lib.rs
@@ -0,0 +1,5 @@
+mod build;
+mod push;
+
+pub use build::*;
+pub use push::*;
--- main/src/lib.rs
+++ main/src/lib.rs
@@ -8,3 +8,9 @@ pub use lib_flutter::*;
 
 #[cfg(feature = "image")]
 pub use lib_image::*;
+
+#[cfg(feature = "git")]
+pub use lib_git::*;
+
+#[cfg(feature = "docker")]
+pub use lib_docker::*;
