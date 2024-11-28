9e9a53b94f4710018ab7c11eca12a826e67c6e16
Mart van Buren <mart@fractic.io>
2024-11-28 09:52:19 +0900

    [networking] Minor: increase SSH wait timeout.

--- lib_networking/src/ssh.rs
+++ lib_networking/src/ssh.rs
@@ -55,7 +55,7 @@ pub async fn wait_until_ssh_available(
     let ip = wait_until_socket_open(pr, hostname, port).await?;
 
     // Next, wait for SSH server to be available.
-    let timeout_duration = Duration::from_secs(2 * 60); // 2 minutes
+    let timeout_duration = Duration::from_secs(5 * 60); // 5 minutes
     let start_time = Instant::now();
     pr.with_status_bar(|mut status_bar| async move {
         let mut last_error = None;
