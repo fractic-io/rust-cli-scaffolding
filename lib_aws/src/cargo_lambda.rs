0bf210509cac3728ddd201f9da7e45ba625828c3
Mart van Buren <mart@fractic.io>
2024-12-02 12:15:34 +0900

    [aws] cargo_lambda: Build to temp dir.

--- lib_aws/src/cargo_lambda.rs
+++ lib_aws/src/cargo_lambda.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
-use lib_core::{CliError, Executor, IOMode, Printer};
+use lib_core::{CliError, CriticalError, Executor, IOMode, Printer};
+use tempfile::tempdir;
 
 use crate::upload_dir_to_s3;
 
@@ -14,6 +15,8 @@ pub async fn cargo_lambda_build_to_s3(
     key_prefix: &str,
 ) -> Result<(), CliError> {
     pr.info("Building binaries...");
+    let target_dir =
+        tempdir().map_err(|e| CriticalError::with_debug("failed to get temp dir", &e))?;
     ex.execute(
         "cargo",
         &[
@@ -21,6 +24,10 @@ pub async fn cargo_lambda_build_to_s3(
             "build",
             "--output-format",
             "zip",
+            "--lambda-dir",
+            target_dir.path().to_str().ok_or_else(|| {
+                CriticalError::new("failed to convert path from tempdir() to string")
+            })?,
             "--release",
             "--arm64",
         ],
@@ -28,14 +35,6 @@ pub async fn cargo_lambda_build_to_s3(
         IOMode::Attach,
     )?;
     pr.info("Uploading zip files to S3...");
-    upload_dir_to_s3(
-        pr,
-        profile,
-        region,
-        bucket,
-        key_prefix,
-        crate_dir.join("target").join("lambda"),
-    )
-    .await?;
+    upload_dir_to_s3(pr, profile, region, bucket, key_prefix, target_dir.path()).await?;
     Ok(())
 }
