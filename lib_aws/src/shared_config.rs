c98e75e099de1a5a5c5a47d9105939044ac35423
Mart van Buren <mart@fractic.io>
2026-02-19 01:50:41 +0900

    [aws] Raise timeouts.

--- lib_aws/src/shared_config.rs
+++ lib_aws/src/shared_config.rs
@@ -1,11 +1,27 @@
-use aws_config::{profile::ProfileFileCredentialsProvider, BehaviorVersion, Region, SdkConfig};
+use std::time::Duration;
+
+use aws_config::{
+    profile::ProfileFileCredentialsProvider, timeout::TimeoutConfig, BehaviorVersion, Region,
+    SdkConfig,
+};
 
 pub(crate) async fn config_from_profile(
     profile: impl Into<String>,
     region: impl Into<String>,
 ) -> SdkConfig {
+    // Important to raise the default timeouts to support large operations
+    // (e.g., large S3 file transfers). In particular, the default 5s connect
+    // timeout can easily trigger on slower connections or with multiple
+    // concurrent operations.
+    let timeout_config = TimeoutConfig::builder()
+        .connect_timeout(Duration::from_secs(30))
+        .operation_attempt_timeout(Duration::from_secs(300))
+        .operation_timeout(Duration::from_secs(3600))
+        .build();
+
     aws_config::defaults(BehaviorVersion::v2026_01_12())
         .region(Region::new(region.into()))
+        .timeout_config(timeout_config)
         .credentials_provider(
             ProfileFileCredentialsProvider::builder()
                 .profile_name(profile)
